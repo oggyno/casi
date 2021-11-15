e27a8512e44dfc618cf6b76e2d634ec1d33e8231
Renaldy <renaldychaw21@gmail.com>
2021-11-15 17:13:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
+
+app.listen('3000', () => {
+    console.log('Server sudah berjalan di port 3000')
+})
